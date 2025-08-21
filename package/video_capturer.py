242fe6b3307cb95c8684da1b762fecb8645fbc25
JiangXiu <JiangXiu11200@gmail.com>
2025-08-21 10:11:11 +0800

    fix (FaceReco): Handling broken pipe error

--- package/video_capturer.py
+++ package/video_capturer.py
@@ -1,18 +1,24 @@
 import threading
 import time
-import traceback
 from multiprocessing import Queue
 
 import cv2
 
+import package.config as config
+
 
 class VideoCapturer:
+    """
+    Video Capturer class to capture video frames from RTSP or webcam. \n
+    Though `vide_queue` to store frames and `stop_event` to signal when to stop capturing.
+    """
+
     def __init__(self, rtsp: str, video_queue: Queue):
         self.rtsp = rtsp
         self.video_queue = video_queue
         self.stop_event = threading.Event()
 
-    def get_video(self):
+    def get_video(self) -> None:
         """
         Get video stream. Initialize VideoCapturer and input video queue and signal queue.
 
@@ -40,14 +46,16 @@ class VideoCapturer:
                 frame = cv2.flip(frame, 1)
                 self.video_queue.put(frame)
                 time.sleep(0.001)
-
-        except Exception:
-            traceback.print_exc()
+        except BrokenPipeError as e:
+            config.logger.debug(f"WebSocket connection broken in VideoCapturer: {e}")
+            config.logger.info("Video capturer closed unexpectedly.")
+        except Exception as e:
+            config.logger.debug(f"Error in VideoCapturer: {e}")
+            config.logger.info("Video capturer closed unexpectedly.")
         finally:
             if self.cap:
                 self.cap.release()
-            print("VideoCapturer stopped.")
+            config.logger.debug("Video capturer thread stopped.")
 
     def stop(self):
-        """通知 thread 停止"""
         self.stop_event.set()
